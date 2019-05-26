99e82f54e8c982e9a335753df146b33b8a9e78f1
Xeon <nickandreyuser@gmail.com>
2019-05-26 20:48:37 +0200

    add menu footer delivery and about us components

--- bears-app/src/app/app-routing.module.ts
+++ bears-app/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {AboutUsComponent} from "./about-us/about-us.component";
+import {DeliveryComponent} from "./delivery/delivery.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "about-us", component: AboutUsComponent},
+  {path: "delivery", component: DeliveryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- bears-app/src/app/app.module.ts
+++ bears-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { DeliveryComponent } from './delivery/delivery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    FooterComponent,
+    AboutUsComponent,
+    DeliveryComponent
   ],
   imports: [
     BrowserModule,
